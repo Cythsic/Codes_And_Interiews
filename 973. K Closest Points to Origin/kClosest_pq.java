e1468f75019b616ed852d28341bfb05bd96c5cc0
JinkeZhao <vivizhao1999@gmail.com>
2022-02-26 15:00:41 -0800

    Problem 973
    973. K Closest Points to Origin
    -- facebook tag
    1. Arrays sort
    2. PriorityQueue

---
+++ 973. K Closest Points to Origin/kClosest_pq.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq=new PriorityQueue<>((o1,o2) -> (o1[0]*o1[0]+o1[1]*o1[1]) - (o2[0]*o2[0]+o2[1]*o2[1]));
+        
+        for(int[] p:points) {
+            pq.offer(p);
+        }
+        
+        int[][] res = new int[k][2];
+        for(int i=0; i<k; i++) {
+            res[i] = pq.poll();
+        }
+        return res;
+    }
+}
