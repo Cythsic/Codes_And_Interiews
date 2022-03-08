132db17f68dcef9d757a6ab278ac23691fc6c044
JinkeZhao <vivizhao1999@gmail.com>
2022-03-07 16:53:50 -0800

    Optimise codes

--- 209. Minimum Size Subarray Sum/minSubArrayLen.java
+++ 209. Minimum Size Subarray Sum/minSubArrayLen.java
@@ -2,19 +2,14 @@
 //TC: O(N)
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int left = 0, right = 0;
-        int res = Integer.MAX_VALUE,sum=0;;
-        
+        int left = 0, right = 0, res = Integer.MAX_VALUE, sum=0;
         for(; right<nums.length; right++) {
-            if(sum<target) {
-                sum += nums[right];
-            }
-            while(sum>=target && left<=right) {
-                res = Math.min(res, right-left+1);
-                sum -= nums[left++];
+            sum+=nums[right];
+            while(sum>=target) {
+                res = Math.min(res,right-left+1);
+                sum-=nums[left++];
             }
         }
-        
-        return res==Integer.MAX_VALUE?0:res;
+        return res==Integer.MAX_VALUE?0 : res;
     }
 }
