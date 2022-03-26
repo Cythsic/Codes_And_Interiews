6178d224206a0b2f7facc8615077b8b9b65048ee
JinkeZhao <vivizhao1999@gmail.com>
2022-03-26 13:36:35 -0700

    Problem 704
    704. Binary Search
    Simple binary search

---
+++ 704. Binary Search/search.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0, end = nums.length-1;
+        while(start < end) {
+            int mid = (end - start)/2 + start;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) start = mid+1;
+            else end = mid-1;
+        }
+        
+        return nums[start] == target? start : -1;
+    }
+}
