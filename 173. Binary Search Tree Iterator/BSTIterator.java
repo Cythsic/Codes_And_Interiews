3975eca94c27b3c5164e69f626d8416cdd511462
JinkeZhao <vivizhao1999@gmail.com>
2022-04-20 15:09:43 -0700

    Problem 173
    173. Binary Search Tree Iterator
    Iterator

---
+++ 173. Binary Search Tree Iterator/BSTIterator.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Iterator<Integer> it;
+    List<Integer> list = new ArrayList<>();
+    public BSTIterator(TreeNode root) {
+        inorder(root);
+        it = list.iterator();
+    }
+    
+    public int next() {
+        return it.hasNext()?it.next() : null;
+    }
+    
+    public boolean hasNext() {
+        return it.hasNext();
+    }
+    
+    public void inorder(TreeNode root) {
+        if(root == null) return;
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
